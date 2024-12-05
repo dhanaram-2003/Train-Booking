10f9ddc827e35873495f8925761d1d1b1d451416
Dhana <dhanaram367@gmail.com>
2024-12-05 14:42:33 +0530

    RAC and WL features are added sucessfully

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     static int numGen = 1;
-    static List<Ticket> booked = new ArrayList<>();
     static int upper = 1;
     static int lower = 1;
     static int middle = 1;
+    static int rac = 1;
+    static int wl = 1;
+    static List<Ticket> booked = new ArrayList<>();
+    static Queue<Ticket> racList = new LinkedList<>();
+    static Queue<Ticket> waitList = new LinkedList<>();
 
     public static void main(String[] args) {
 
@@ -16,7 +18,7 @@ public class Main {
         while(true){
             System.out.println("----- Welcome to Train Booking -----");
             System.out.println("1.Booking");
-            System.out.println("2.Display");
+            System.out.println("2.Display Ticket Details");
             System.out.println("3.Cancel");
             System.out.println("4.Exit");
             System.out.println("------------------------------------");
@@ -30,7 +32,7 @@ public class Main {
                     break;
 
                 case 2:
-                    viewTicketDetails();
+                    displayTicketDetails();
                     break;
 
                 case 3:
@@ -119,30 +121,87 @@ public class Main {
             }
             return true;
         }
+        else if(racList.size()<1){
+            System.out.print("Enter Your Name: ");
+            String name = sc.nextLine();
+            System.out.print("from: ");
+            String from = sc.nextLine();
+            System.out.print("to: ");
+            String to = sc.nextLine();
+            char pref = 'l';
+            boolean flag=true;
+            while(flag){
+                System.out.print("Berth Preference [U/M/L]: ");
+                pref = sc.nextLine().charAt(0);
+                if(Character.toLowerCase(pref) != 'u' && Character.toLowerCase(pref) != 'm'&& Character.toLowerCase(pref) != 'l'){
+                    System.out.println("Enter valid berth!!!");
+                }
+                else flag=false;
+            }
+            Ticket ticket = new Ticket(numGen++,name,from,to,Character.toLowerCase(pref));
+            racList.add(ticket);
+            System.out.println("RAC Ticket Alotted");
+            return true;
+        }
+        else if(waitList.size()<1){
+            System.out.print("Enter Your Name: ");
+            String name = sc.nextLine();
+            System.out.print("from: ");
+            String from = sc.nextLine();
+            System.out.print("to: ");
+            String to = sc.nextLine();
+            char pref = 'l';
+            boolean flag=true;
+            while(flag){
+                System.out.print("Berth Preference [U/M/L]: ");
+                pref = sc.nextLine().charAt(0);
+                if(Character.toLowerCase(pref) != 'u' && Character.toLowerCase(pref) != 'm'&& Character.toLowerCase(pref) != 'l'){
+                    System.out.println("Enter valid berth!!!");
+                }
+                else flag=false;
+            }
+            Ticket ticket = new Ticket(numGen++,name,from,to,Character.toLowerCase(pref));
+            waitList.add(ticket);
+            System.out.println("Added to Waiting List");
+            return true;
+        }
         else{
             System.out.println("Bookings full !!!");
             return false;
         }
     }
 
-    public static void viewTicketDetails(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Ticket Number: ");
-        int ticketNum = sc.nextInt();
-
-        Ticket ticket = null;
-        for(Ticket t:booked){
-            if(t.getNum() == ticketNum)
-                ticket = t;
-        }
-        if(ticket != null){
+    public static void displayTicketDetails(){
+        System.out.println("-------- Confirmed Bookings -------");
+        for(Ticket ticket:booked){
             System.out.println("Ticket Number:" + ticket.getNum());
             System.out.println("Name:" + ticket.getpName());
             System.out.println("From:" + ticket.getFrom());
             System.out.println("T0:" + ticket.getTo());
             System.out.println("Berth:"+ ticket.getBerthPref());
         }
-        else System.out.println("Ticket Not Available !!!");
+        System.out.println("-------- RAC Bookings -------");
+        if(!racList.isEmpty()){
+            for(Ticket ticket:racList){
+                System.out.println("Ticket Number:" + ticket.getNum());
+                System.out.println("Name:" + ticket.getpName());
+                System.out.println("From:" + ticket.getFrom());
+                System.out.println("T0:" + ticket.getTo());
+                System.out.println("Berth:"+ ticket.getBerthPref());
+            }
+        }
+
+        System.out.println("-------- Waiting Bookings -------");
+        if (!waitList.isEmpty()){
+            for (Ticket ticket : waitList) {
+                System.out.println("Ticket Number:" + ticket.getNum());
+                System.out.println("Name:" + ticket.getpName());
+                System.out.println("From:" + ticket.getFrom());
+                System.out.println("T0:" + ticket.getTo());
+                System.out.println("Berth:" + ticket.getBerthPref());
+            }
+        }
+
     }
 
     public static void cancelTicket(){
@@ -150,9 +209,10 @@ public class Main {
         System.out.print("Enter Your Ticket Number: ");
         int ticketNum = sc.nextInt();
         boolean flag = false;
+        char pref = 'l';
         for(Ticket t:booked){
             if(t.getNum() == ticketNum) {
-                char pref = t.getBerthPref();
+                pref = t.getBerthPref();
                 if(pref == 'u') upper++;
                 else if(pref == 'm') middle++;
                 else lower++;
@@ -160,8 +220,20 @@ public class Main {
                 break;
             }
         }
-        if(flag) System.out.println("Cancelled Successfully!");
-        else System.out.println("Invalid Ticket Detail !!!");
-
+        if(flag){
+            System.out.println("Cancelled Successfully!");
+            if(!racList.isEmpty()) {
+                Ticket t = racList.poll();
+                t.setBerthPref(pref);
+                booked.add(t);
+                if (pref == 'u') upper--;
+                else if (pref == 'm') middle--;
+                else lower--;
+                if(!waitList.isEmpty())
+                    racList.add(waitList.poll());
+            }
+        }
+        else
+            System.out.println("Invalid Ticket Detail !!!");
     }
 }
